328bc5255374d258f3dffa9f819b336d7aac0fcd
Brad Gessler <bradgessler@gmail.com>
2025-09-03 14:27:15 -0700

    Copy instance methods from controller to instance of Phlex view

--- app/views/application/index.html.rb
+++ app/views/application/index.html.rb
@@ -2,6 +2,8 @@ Views::Layouts::Application(
   title: "Home",
   subtitle: "Welcome to PhlexML"
 ){
+  plain "The greeting is #{@greeting}"
+
   Button { "Click me" }
 
   main {
--- config/initializers/phlexml.rb
+++ config/initializers/phlexml.rb
@@ -6,15 +6,31 @@ module Phlex::Markup
       src = source || template.source
 
       <<~RUBY
-        # Define an anonymous subclass of Phlex::HTML
+        # Define an anonymous subclass of your Phlex base
         __phlex_class__ = Class.new(::Views::Base) do
           def view_template
             #{src}
           end
         end
 
-        # Instantiate and render it
-        __phlex_class__.new.render_in(self).to_s
+        # Instantiate
+        __phlex_instance__ = __phlex_class__.new
+
+        # Copy controller assigns (only user-defined ivars) into the component.
+        # Prefer `view_assigns` (Rails filters internals), fallback to `assigns`.
+        __assigns__ = respond_to?(:view_assigns) ? view_assigns : assigns
+        __assigns__.each do |k, v|
+          __ivar__ = :"@\#{k}"
+          if __phlex_instance__.instance_variable_defined?(__ivar__)
+            raise ArgumentError,
+              "Refusing to overwrite \#{__ivar__} on \#{__phlex_class__}. "\
+              "It was already set by the component before assigns were applied."
+          end
+          __phlex_instance__.instance_variable_set(__ivar__, v)
+        end
+
+        # Render
+        __phlex_instance__.render_in(self).to_s
       RUBY
     end
   end
